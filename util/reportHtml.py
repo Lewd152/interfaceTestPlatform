95d554c7b4e64e7251801ba9cc62cc07153e3fa1
Lewd152 <1737678325@qq.com>
2020-11-19 18:25:14 +0800

    删除HTML

--- util/reportHtml.py
+++ util/reportHtml.py
@@ -1,24 +1,24 @@
-# coding=utf-8
-import unittest
-import HTMLTestRunnerNew
-import time
-
-#生成报告  传入case
-def runCaseReportHtml(caseModel,title,tester,description):
-    # 存储测试用例
-    suite = unittest.TestSuite()
-
-    # 创建一个加载器
-    loader = unittest.TestLoader()
-
-    # 从测试模块里面去找测试用例
-    suite.addTest(loader.loadTestsFromModule(caseModel))
-    datetime = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    # 测试报告
-    with open(str(datetime)+"report.html", 'wb') as file:
-        runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file,
-                                                  verbosity=2,
-                                                  description=description,
-                                                  title=title,
-                                                  tester=tester)
-        runner.run(suite)
+# # coding=utf-8
+# import unittest
+# import HTMLTestRunnerNew
+# import time
+#
+# #生成报告  传入case
+# def runCaseReportHtml(caseModel,title,tester,description):
+#     # 存储测试用例
+#     suite = unittest.TestSuite()
+#
+#     # 创建一个加载器
+#     loader = unittest.TestLoader()
+#
+#     # 从测试模块里面去找测试用例
+#     suite.addTest(loader.loadTestsFromModule(caseModel))
+#     datetime = time.strftime("%Y%m%d%H%M%S", time.localtime())
+#     # 测试报告
+#     with open(str(datetime)+"report.html", 'wb') as file:
+#         runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file,
+#                                                   verbosity=2,
+#                                                   description=description,
+#                                                   title=title,
+#                                                   tester=tester)
+#         runner.run(suite)
